323f67c1fee72fefda320b65fc54e1a226952855
Liz4160 <utm180159ti@ut-morelia.edu.mx>
2022-07-23 12:44:59 -0500

    admin

---
+++ prueba/registros/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Alumnos (models.Model): #Define la setructura de nuestra tabla
+    matricula = models.CharField(max_length=12)#Texto corto
+    nombre = models.TextField()#Texto largo
+    carrera = models.TextField
+    turno = models.CharField(max_length=10)
+    created = models.DateTimeField(auto_now_add=True)#Fecha y Tiempo
+    updated = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    verbose_name = "Alumno"
+    verbose_name_plural = "Alumnos"
+    ordering = ["-created"]
+    #el menos indica que se ordenara del más reciente al mas viejo
+
+def __str__(self):
+    return self.nombre
+    #Indica que se mostrára el nombre como valor en la tabla
